f8d83d6748aaa75d3209d2573d076014aa81c9ee
miglio000 <61146382+miglio000@users.noreply.github.com>
2020-11-17 16:55:59 +0100

    Add files via upload
    Prova delle classi Input e Output

---
+++ PesoEtàNome.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Input_Output;
+
+namespace peso_età_nome
+{
+    class PesoEtàNome
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Inserisci il tuo nome: ");
+            string nome = Input.InserisciStringa();
+            Output.StampaStringa("Inserici età e peso: ");
+            int età = Input.InserisciIntero();
+            double peso = Input.InserisciDouble();
+            string condizione = Input.InserisciBool();
+
+            Output.StampaStringa($"Sei {nome}, ho {età} anni e peso: {peso} kg.{condizione}");
+        }
+    }
+}
