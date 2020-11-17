2cf51c1aa1ab3e63b0bc1b83f622da18f2dca980
miglio000 <gabrielemiglio23@gmail.com>
2020-11-17 16:51:46 +0100

    Add project files.

---
+++ Input.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Input_Output
+{
+    public class Input
+    {
+        public static int InserisciIntero()
+        {
+            Console.Write("Inserisci un numero intero:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        
+            
+        public static double InserisciDouble()
+        {
+            Console.Write("Inserisci un numero decimale:");
+            double n = Convert.ToDouble(Console.ReadLine());
+            return n;
+      
+        }
+        public static string InserisciStringa()
+        {
+            Console.Write("Inserisci una stringa:");
+            string s = Convert.ToString(Console.ReadLine());
+            return s;
+        }
+        public static string InserisciBool()
+        {
+            Console.Write("Inserire vero o falso:");
+            string condizione = Console.ReadLine();
+            if (condizione == "true")
+            {
+                Console.WriteLine("true");
+            }
+            else if(condizione == "false")
+            {
+                Console.WriteLine("false");
+            }
+            return condizione;
+        }
+    }
+}
---
+++ Output.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Input_Output
+{
+    public class Output
+    {
+        public static void StampaIntero(int n)
+        {
+            Console.WriteLine($"Il numero intero è {n}");
+        }
+        public static void StampaDouble(double n)
+        {
+            Console.WriteLine($"Il numero decimale è {n}");          
+        } 
+        public static void StampaStringa(string s)
+        {
+            Console.WriteLine(s);
+        }
+        public static void StampaBool(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
